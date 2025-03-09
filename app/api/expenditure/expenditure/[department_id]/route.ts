44cc7cef667c3103505938a2d918db6c2d79b078
Yeet2042 <arpeggiokongo@gmail.com>
2025-03-09 09:28:02 +0700

    Refactor GET route for fetching expenditures by department; improve code formatting and error handling

--- app/api/expenditure/expenditure/[department_id]/route.ts
+++ app/api/expenditure/expenditure/[department_id]/route.ts
@@ -1,19 +1,25 @@
-import { prisma } from '@/lib/prisma';
-import { NextResponse } from 'next/server';
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
 
-export async function GET(req: Request, { params } : { params: Promise<{ department_id: string }> }) {
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ department_id: string }> }
+) {
   try {
     const { department_id } = await params;
 
     const expenditures = await prisma.expenditure.findMany({
       where: {
-        departmentId: Number(department_id)
-      }
-    })
+        departmentId: Number(department_id),
+      },
+    });
 
     return NextResponse.json({ expenditures });
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ error: "เกิดข้อผิดพลาดในการดึงข้อมูลปีงบประมาณทั้งหมด" }, { status: 500 });
+    return NextResponse.json(
+      { error: "เกิดข้อผิดพลาดในการดึงข้อมูลปีงบประมาณทั้งหมด" },
+      { status: 500 }
+    );
   }
 }
